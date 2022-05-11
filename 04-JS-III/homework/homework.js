b4bdc2d1185a2a0206d7e57ff82fc25684f9b190
Martinmessner <martinmessner95@gmail.com>
2022-05-11 18:51:23 -0300

    terminado

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -158,7 +158,13 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  } 
+  let num = n.toString()
+  if(num.charAt(0) === "9"){
+    return true
+  }
+  return false
+}
+   
   
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
@@ -202,7 +208,7 @@ function mayorACien(array) {
   
 }
 
-// npm test JSIII.test.js
+// 
 function breakStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
   //Guardar cada nuevo valor en un array. 
@@ -211,21 +217,23 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
-  array = [];
- 	suma = 0;
-  for (i = 0;i < 10; i+2){
-    sum = suma + numero
-  } 
-  if (i < 10) {
-   return "Se interrumpio la ejecucion"
+  let array = [];
+  let suma = numero;
+  for(let i = 0; i < 10; i++) {
+    suma = suma + 2;
+    if(suma === i) break;
+    else {
+    array.push(suma);
+    }
+  }
+  if(suma < 10) {
+    return 'Se interrumpió la ejecución';
+  }
+  else {
+      return array;
   }
 }
-  
-
-      
-  
-
-
+// npm test JSIII.test.js
 function continueStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
   //Guardar cada nuevo valor en un array.    
@@ -233,7 +241,20 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  let array = [];
+  let suma = numero;
+  for(let i= 0; i<10; i++) {
+    if(i === 5) continue;
+    else {
+      suma = suma + 2;
+      array.push(suma);
+    }
+  }
+  return array;
 }
+
+
+
 
 // No modificar nada debajo de esta línea
 // --------------------------------
